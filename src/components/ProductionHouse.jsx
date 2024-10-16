d35fc084b4d4b57b6119bd719b01abb45654df84
idil-abdi <edilabdiaziz@gmail.com>
2024-10-16 11:40:29 +0100

    adding img and video

---
+++ src/components/ProductionHouse.jsx
@@ -0,0 +1,19 @@
+import disney from './../assets/img/disney.png'
+import marvel from './../assets/img/marvel.png'
+import nationalG from './../assets/img/nationalG.png'
+import pixar from './../assets/img/pixar.png'
+import starwar from './../assets/img/starwar.png'
+
+import disneyV from '../assets/video/disney.mp4'
+import marvelV from '../assets/video/marvel.mp4'
+import nationalGeoV from '../assets/video/nationalGeo.mp4'
+import pixarV from '../assets/video/pixar.mp4'
+import starwarsV from '../assets/video/star-wars.mp4'
+
+const ProductionHouse = () => {
+    return (
+        <div>ProductionHouse</div>
+    )
+}
+
+export default ProductionHouse
