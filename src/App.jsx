3e60d3f915e6c0a735a4f9be6323658f8c49991b
idil-abdi <edilabdiaziz@gmail.com>
2024-10-16 11:00:34 +0100

    slider completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header'
+import Slider from './components/Slider'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <div> 
         <Header/>
+        <Slider/>
       </div>
     </>
   )
---
+++ src/components/Slider.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+import GlobalApi from "../Services/GlobalApi";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original/';
+const screenWidth = window.innerWidth;
+
+const Slider = () => {
+    const [movieList, setMovieList] = useState([]);
+    const elementRef = useRef();
+    useEffect(() => {
+        getTrendingMovies()
+    },[])
+    const getTrendingMovies = () => {
+        GlobalApi.getTrendingVideos.then(resp =>{
+            //console.log(resp.data.results);
+            setMovieList(resp.data.results)
+        })
+    }
+
+    const sliderRight = (element) => {
+        element.scrollLeft += screenWidth-110
+    }
+    const sliderLeft = (element) => {
+        element.scrollLeft -= screenWidth-110
+    }
+
+    return (
+        <div>
+            <HiChevronLeft  onClick={() => sliderLeft(elementRef.current)} className="hidden md:block text-white text-[30px] absolute mx-8 mt-[200px] cursor-pointer"/>
+            <HiChevronRight onClick={() => sliderRight(elementRef.current)} className="hidden md:block text-white text-[30px] absolute mx-8 mt-[200px] cursor-pointer right-0"/>
+            <div ref={elementRef} className="flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth">
+                {movieList.map((item, index) => (
+                    <img key={index} className="min-w-full md:h-[400px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in-out" src={IMAGE_BASE_URL+item.backdrop_path} alt="backdrop" />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Slider
